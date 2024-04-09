9efa291ae5b2a5c7f2fe1b44d4ed8c62f9c2b478
Daniil <bombyascher@gmail.com>
2024-04-09 19:28:36 +0300

    Added one-to-many relationship between urls and checks entities.

--- app/src/main/resources/h2.sql
+++ app/src/main/resources/h2.sql
@@ -1,19 +1,16 @@
-DROP TABLE IF EXISTS urls;
-
-CREATE TABLE urls (
+CREATE TABLE IF NOT EXISTS urls (
     id bigint PRIMARY KEY AUTO_INCREMENT,
     name varchar(255) NOT NULL,
     created_at timestamp
 );
 
-DROP TABLE IF EXISTS url_checks;
-
-CREATE TABLE url_checks (
+CREATE TABLE IF NOT EXISTS url_checks (
     id bigint PRIMARY KEY AUTO_INCREMENT,
     url_id bigint,
     status_code int,
     h1 varchar(255) NOT NULL,
     title varchar(255),
     description text,
-    created_at timestamp
+    created_at timestamp,
+    FOREIGN KEY (url_id) REFERENCES urls(id)
 );
