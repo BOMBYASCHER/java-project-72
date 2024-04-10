edd87e7d8bfc6367c3e83699b1e6619dd1c834bd
Daniil <bombyascher@gmail.com>
2024-04-10 14:20:49 +0300

    Attempting to pass hexlet tests

--- app/src/main/resources/postgre.sql
+++ app/src/main/resources/postgre.sql
@@ -1,16 +1,19 @@
-CREATE TABLE IF NOT EXISTS urls (
-    id bigint PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+DROP TABLE IF EXISTS urls;
+
+CREATE TABLE urls (
+    id bigint PRIMARY KEY AUTO_INCREMENT,
     name varchar(255) NOT NULL,
     created_at timestamp
 );
 
-CREATE TABLE IF NOT EXISTS url_checks (
-    id bigint PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+DROP TABLE IF EXISTS url_checks;
+
+CREATE TABLE url_checks (
+    id bigint PRIMARY KEY AUTO_INCREMENT,
     url_id bigint,
     status_code int,
     h1 varchar(255) NOT NULL,
     title varchar(255),
     description text,
-    created_at timestamp,
-    FOREIGN KEY (url_id) REFERENCES urls(id)
+    created_at timestamp
 );
